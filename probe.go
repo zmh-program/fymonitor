7f72f8b4ab7bb7333c6572ce5576ef7f8f1a44a6
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-06-01 16:43:33 +0800

    update backend

--- probe.go
+++ probe.go
@@ -2,101 +2,103 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
-	"fmt"
-	_ "github.com/mattn/go-sqlite3"
-	"github.com/rs/cors"
 	"log"
 	"net/http"
-	"sync"
 	"time"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
 )
 
-var websites = []string{
-	"https://www.google.com",
-	"https://www.yahoo.com",
-	"https://www.bing.com",
-}
-
-type Result struct {
-	Website   string
-	Status    string
-	Timestamp string
-}
-
-func monitor(wg *sync.WaitGroup, db *sql.DB, url string) {
-	defer wg.Done()
-	for {
-		status := "down"
-		resp, err := http.Get(url)
-		if err != nil {
-			log.Printf("Error for %s: %s", url, err)
-		} else if resp.StatusCode == 200 {
-			status = "up"
-		}
-
-		_, err = db.Prepare("INSERT INTO monitor_results(website, status, timestamp) values(?,?,?)")
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%s is %s\n", url, status)
-
-		time.Sleep(5 * time.Second)
-	}
-}
-
-func statusHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
-	rows, _ := db.Query("SELECT website, status, timestamp FROM monitor_results ORDER BY timestamp DESC")
-	defer rows.Close()
-
-	results := make([]*Result, 0)
-
-	for rows.Next() {
-		result := new(Result)
-		err := rows.Scan(&result.Website, &result.Status, &result.Timestamp)
-		if err != nil {
-			return
-		}
-		results = append(results, result)
-	}
-
-	err := json.NewEncoder(w).Encode(results)
-	if err != nil {
-		return
-	}
+type MonitorData struct {
+	URL    string `json:"url"`
+	Status string `json:"status"`
 }
 
 func main() {
-	db, _ := sql.Open("sqlite3", "./monitor.db")
-	defer db.Close()
-
-	sqlStmt := `
-	CREATE TABLE IF NOT EXISTS monitor_results (
-		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-		website TEXT NOT NULL,
-		status TEXT NOT NULL,
-		timestamp DATETIME NOT NULL
-	);
-	`
-	_, err := db.Exec(sqlStmt)
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
 	if err != nil {
-		log.Printf("%q: %s\n", err, sqlStmt)
-		return
+		log.Fatalf("Error reading config file, %s", err)
 	}
 
-	var group sync.WaitGroup
-
-	for _, website := range websites {
-		group.Add(1)
-		go monitor(&group, db, website)
+	// Establish database connection
+	db, err := sql.Open("mysql", viper.GetString("database.dsn"))
+	if err != nil {
+		log.Fatal("Error connecting to database: ", err)
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
-		statusHandler(w, r, db)
+	// Create table if it doesn't exist
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS monitor (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		url VARCHAR(256) NOT NULL,
+		status VARCHAR(64) NOT NULL DEFAULT 'unknown',
+		stamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+	)
+	`)
+	if err != nil {
+		log.Fatal("Error creating table: ", err)
+	}
+
+	r := gin.Default()
+
+	r.POST("/monitor", func(c *gin.Context) {
+		var newMonitorData MonitorData
+		if err := c.ShouldBindJSON(&newMonitorData); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		_, err := db.Exec("INSERT INTO monitor (url) VALUES (?)", newMonitorData.URL)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.Status(http.StatusCreated)
 	})
 
-	handler := cors.Default().Handler(mux)
+	r.PUT("/monitor/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var updatedMonitorData MonitorData
+		if err := c.ShouldBindJSON(&updatedMonitorData); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		_, err := db.Exec("UPDATE monitor SET status = ? WHERE id = ?", updatedMonitorData.Status, id)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.Status(http.StatusOK)
+	})
 
-	log.Fatal(http.ListenAndServe(":8080", handler))
+	go func() {
+		for range time.Tick(time.Minute) {
+			rows, err := db.Query("SELECT id, url FROM monitor")
+			if err != nil {
+				log.Println("Error querying database: ", err)
+				continue
+			}
+			for rows.Next() {
+				var id int
+				var url string
+				if err := rows.Scan(&id, &url); err != nil {
+					log.Println("Error scanning row: ", err)
+					continue
+				}
+				resp, err := http.Get(url)
+				if err != nil {
+					log.Println("Error checking URL: ", err)
+					db.Exec("UPDATE monitor SET status = ? WHERE id = ?", "down", id)
+					continue
+				}
+				resp.Body.Close()
+				db.Exec("UPDATE monitor SET status = ?, stamp = ? WHERE id = ?", "up", time.Now(), id)
+			}
+		}
+	}()
+
+	r.Run()
 }
